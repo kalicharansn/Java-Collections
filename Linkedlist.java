b4ba8015b35c8446becb84b70e8527e118418ae5
kalicharansn <49032356+kalicharansn@users.noreply.github.com>
2019-03-28 21:18:10 +0530

    Initial commit

---
+++ Linkedlist.java
@@ -0,0 +1,21 @@
+package collections;
+
+import java.util.LinkedList;
+
+public class Linkedlist {
+
+	public static void main(String[] args) {
+		//used for add and delete, search not good
+		LinkedList<Integer> details = new LinkedList<Integer>();
+		details.add(7);
+		details.add(8);
+		details.add(9);
+		details.removeFirst();
+		details.removeLast();
+		System.out.println(details);
+		for (Integer integer : details) {
+			System.out.println(integer);
+		}
+	}
+
+}
